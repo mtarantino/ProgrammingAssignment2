cfab06556c49ec9aa56ddf5a927d09ceb3f9ae0a
Michael Tarantino <tarantin@adobe.com>
2015-08-21 13:13:30 +0200

    Assignemnt 2: implement makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,11 @@
-# Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse
-# of a matrix rather than compute it repeatedly (there are also alternatives to matrix inversion 
-# that we will not discuss here). Your assignment is to write a pair of functions that cache the inverse 
-# of a matrix.
-
-# Write the following functions:
-
-# makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
-# cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
-
-# Computing the inverse of a square matrix can be done with the solve function in R. For example, if X
-# is a square invertible matrix, then solve(X) returns its inverse.
-# For this assignment, assume that the matrix supplied is always invertible.
-
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+#############################################
 # makeCacheMatrix(x)
 # Declare the cache Matric functions and return the list of available object function
 # Parameters :
 #   x : a square invertible matrix
 # Retruns :
 #   List of functions 
-
+#############################################
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
     
@@ -62,7 +44,7 @@ makeCacheMatrix <- function(x = matrix()) {
          getreverse = getreverse)
 }
 
-
+#############################################
 # cacheSolve(x)
 # Creates a reverse matrix and cache it
 # Parameters
@@ -70,6 +52,7 @@ makeCacheMatrix <- function(x = matrix()) {
 #   ... : parameters given to the solve() function applied on the matrix
 # Returns
 #   the reserve matrix of makeCacheMatrix
+#############################################
 cacheSolve <- function(x, ...) {
     # Return the cache reverse matrix or NULL if it hasn't been calculated
     m <- x$getreverse()
